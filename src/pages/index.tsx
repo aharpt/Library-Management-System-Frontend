09dc57c48ca3b798735502adce80fa3f2609fc1f
Aaron Harpt <aaron.harpt@gmail.com>
2024-10-11 17:44:48 -0500

    Added the Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
-import { Typography } from "@mui/material";
+import { Typography, Grid2 as Grid, Link } from "@mui/material";
 import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <Typography variant="h3">Home Page</Typography>
+      <Grid container spacing={2}>
+        <Grid offset={1} size={10}>
+          <Typography variant="h4">Home Page</Typography>
+          <hr />
+        </Grid>
+        <Grid offset={1} size={10}>
+          <Typography variant="body2">Welcome to our Library! To search our library, click <Link href="/search">here.</Link></Typography>
+        </Grid>
+      </Grid>
     </>
   );
 }
